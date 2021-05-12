0d931271aa4287096e1fde399eeea54360f535a5
rnick1 <rnick0260@gmail.com>
2021-05-11 19:07:43 -0700

    Finished linking images.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Project from "../components/Project";
 import projects from "../projects.json";
+import workoutTracker from "../images/workoutTracker.PNG"
+import germPod from "../images/germPod.PNG"
+import eCommerceBackend from "../images/eCommerceBackend.PNG"
+import readmeGenerator from "../images/readmeGenerator.PNG"
+import mealPlanner from "../images/mealPlanner.PNG"
+import passwordGenerator from "../images/passwordGenerator.PNG"
 
 function Portfolio() {
   return (
@@ -14,37 +20,37 @@ function Portfolio() {
           title={projects[0].title}
           deployed_link={projects[0].deployed_link}
           github_link={projects[0].github_link}
-          image={projects[0].image}
+          image={workoutTracker}
         />
         <Project
           title={projects[1].title}
           deployed_link={projects[1].deployed_link}
           github_link={projects[1].github_link}
-          image={projects[1].image}
+          image={germPod}
         />
         <Project
           title={projects[2].title}
           deployed_link={projects[2].deployed_link}
           github_link={projects[2].github_link}
-          image={projects[2].image}
+          image={eCommerceBackend}
         />
         <Project
           title={projects[3].title}
           deployed_link={projects[3].deployed_link}
           github_link={projects[3].github_link}
-          image={projects[3].image}
+          image={readmeGenerator}
         />
                 <Project
           title={projects[4].title}
           deployed_link={projects[4].deployed_link}
           github_link={projects[4].github_link}
-          image={projects[4].image}
+          image={mealPlanner}
         />
         <Project
           title={projects[5].title}
           deployed_link={projects[5].deployed_link}
           github_link={projects[5].github_link}
-          image={projects[5].image}
+          image={passwordGenerator}
         />
         <div className="col mb-4">
         </div>
